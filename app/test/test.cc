f88adaabdf9623bc2f4aae75583f78a5ce971eae
dokee <dokee.39@gmail.com>
2025-03-25 01:07:47 +0800

    fix: update exec module (remove tag_invoke)

--- app/test/test.cc
+++ app/test/test.cc
@@ -3,16 +3,16 @@ import exec;
 
 struct my_receiver {
     template <typename... Ts>
-    friend void tag_invoke(exec::set_value_tag, my_receiver&&, Ts&&... values) noexcept {
+    void set_value(my_receiver&&, Ts&&... values) noexcept {
         std::println("my_receiver set value in thread {}", std::this_thread::get_id());
         ((std::cout << values << " "), ...);
         std::println();
     }
     template <typename E>
-    friend void tag_invoke(exec::set_error_tag, my_receiver&&, E&&) noexcept {
+    void set_error(my_receiver&&, E&&) noexcept {
         std::println("my_receiver set error");
     }
-    friend void tag_invoke(exec::set_done_tag, my_receiver&&) noexcept {
+    void set_done(my_receiver&&) noexcept {
         std::println("my_receiver set done");
     }
 };
@@ -23,7 +23,7 @@ int main() {
     exec::sender auto s = exec::schedule(sched)
       | exec::then([]() static { std::println("Hello World"); });
 
-    auto op = exec::connect(s, my_receiver{});
+    exec::operation_state auto op = exec::connect(s, my_receiver{});
     exec::start(op);
 }
 
