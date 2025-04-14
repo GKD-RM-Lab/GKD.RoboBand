2f62973d70eca8de1cf71a34fcd455dd4cf4a09e
dokee <dokee.39@gmail.com>
2025-04-15 04:09:30 +0800

    feat: add io::adaptor

--- app/test/test.cc
+++ app/test/test.cc
@@ -2,35 +2,47 @@ import std;
 import robo;
 
 using namespace std::chrono_literals;
+using namespace robo::spt::byte_literals;
 
 constexpr auto context = robo::spt::thread_context::make();
-constexpr robo::io::Serial::Info serial_info {
+constexpr robo::io::Serial::Info serial {
     .name = "/dev/serial/by-id/usb-DM-IMU_DM-IMU_USB_CDC_2025021200-if00",
     .context = context[0],
     .rx_idle_duration = 50us,
-    .rx_buffer_size = 57,
+    .rx_buffer_size = 19,
     .baud_rate = 4000000
 };
 
+constexpr std::array key1 { 0x55_b, 0xaa_b, 0x01_b, 0x01_b };
+constexpr std::array key2 { 0x55_b, 0xaa_b, 0x01_b, 0x02_b };
+constexpr std::array key3 { 0x55_b, 0xaa_b, 0x01_b, 0x03_b };
+
 int main() {
-    auto& serial = robo::io::Serial::get(serial_info);
-    serial.register_callback([&serial] {
-        auto rx_bytes = serial.get_rx_bytes();
-        // if (rx_bytes.size() != 57) {
+    auto callback = [](auto key) {
+        auto rx_bytes = robo::io::get_rx_bytes(serial);
+        // if (rx_bytes.size() != 19) {
         if (true) {
             std::println("{}", std::chrono::system_clock::now());
-            std::print("{:3} bytes received: ", rx_bytes.size()); 
+            std::print("key 0x{:02x}: {:3} bytes received: ", 
+                std::to_integer<int>(key[3]), rx_bytes.size()); 
             for (auto e : rx_bytes) {
                 std::print("{:02x} ", std::to_integer<unsigned int>(e));
             }
             std::print("\n\n"); 
         }
-    });
+    };
 
-    auto send_task = [&serial] {
+    robo::io::register_callback<robo::io::prefix_key<4>>(
+        serial, key1, [&] { callback(key1); });
+    robo::io::register_callback<robo::io::prefix_key<4>>(
+        serial, key2, [&] { callback(key2); });
+    robo::io::register_callback(
+        serial, robo::io::prefix_key(key3), [&] { callback(key3); });
+
+    auto send_task = [] {
         while (true) {
             std::array<std::byte, 100> arr {};
-            serial.send(arr);
+            robo::io::send(serial, arr);
             std::this_thread::sleep_for(1ms);
         }
     };
--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,12 @@
 add_rules("plugin.compile_commands.autoupdate", { outputdir = "build" })
-add_rules("mode.debug", "mode.releasedbg", "mode.release")
+add_rules("mode.debug")
+add_rules("mode.releasedbg")
+add_rules("mode.release")
 
 set_policy("build.warning", true)
 set_warnings("allextra", "pedantic")
 add_cxxflags("-fexperimental-library")
+-- set_optimize("fastest");
 
 if is_mode("debug") then
     add_cxxflags("-ftrapv")
