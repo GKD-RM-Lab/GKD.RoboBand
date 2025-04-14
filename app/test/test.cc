40ac4579b8d099d6593042462572f5f6662d8757
dokee <dokee.39@gmail.com>
2025-04-14 09:53:21 +0800

    feat: add multiton

--- app/test/test.cc
+++ app/test/test.cc
@@ -1,18 +1,23 @@
 import std;
 import robo;
 
+using namespace std::chrono_literals;
+
+constexpr auto context = robo::spt::thread_context::make();
+constexpr robo::io::Serial::Info serial_info {
+    .name = "/dev/serial/by-id/usb-DM-IMU_DM-IMU_USB_CDC_2025021200-if00",
+    .context = context[0],
+    .rx_idle_duration = 50us,
+    .rx_buffer_size = 57,
+    .baud_rate = 4000000
+};
 
 int main() {
-    using namespace std::chrono_literals;
-
-    robo::spt::thread_context context;
-    context.add_task([] {});
-
-    robo::io::Serial serial { context, "/dev/serial/by-id/usb-DM-IMU_DM-IMU_USB_CDC_2025021200-if00", 50us, 57, 4000000 };
-    serial.register_callback([&serial] { 
+    auto& serial = robo::io::Serial::get(serial_info);
+    serial.register_callback([&serial] {
         auto rx_bytes = serial.get_rx_bytes();
-        if (rx_bytes.size() != 57) {
-        // if (true) {
+        // if (rx_bytes.size() != 57) {
+        if (true) {
             std::println("{}", std::chrono::system_clock::now());
             std::print("{:3} bytes received: ", rx_bytes.size()); 
             for (auto e : rx_bytes) {
@@ -36,7 +41,7 @@ int main() {
     std::jthread send_thread5 { send_task };
     std::jthread send_thread6 { send_task };
 
-    context.start();
+    robo::spt::thread_context::start();
     std::this_thread::sleep_for(std::chrono::hours::max());
 }
 
